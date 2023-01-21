7224a3d6ed9993577ed44d04ec38dd0f273213c0
Peter Pumpkineater <n1-dev+pumpkineater6996@simplelogin.co>
2023-01-21 01:47:15 -0500

    Revert "Temp remove prisma"
    This reverts commit 11c08fbc20c7cc1d3c6e0993713a94241eedfffa.

---
+++ src/api/src/prisma/migrations/20220808160320_/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE `posts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `content` TEXT NULL,
+    `img` VARCHAR(191) NULL,
+    `author_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` VARCHAR(191) NOT NULL DEFAULT 'subscriber',
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ src/api/src/prisma/seed.ts
@@ -0,0 +1,84 @@
+const { PrismaClient } = require('./index.js');
+const { faker } = require('@faker-js/faker');
+
+const prisma = new PrismaClient();
+const postsTable = prisma.post;
+const usersTable = prisma.user;
+
+class User {
+    public username: string;
+    public email: string;
+    public password: string;
+    public role: string;
+
+    constructor(username: string, email: string, password: string, role: string) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
+
+class Post {
+    public title: string;
+    public content: string;
+    public img: string;
+    public author_id: number;
+
+    constructor(title: string, content: string, img: string, author_id: number) {
+        this.title = title;
+        this.content = content;
+        this.img = img;
+        this.author_id = author_id;
+    }
+}
+
+const seedMany = (table: any) => async (data: any) => await table.createMany({
+    data
+});
+
+const generateUsers = (count: number) => {
+    const { userName, email, password } = faker.internet;
+    const role = 'subscriber';
+
+    let users = [];
+
+    for (let index = 0; index < count; index++) {
+        users.push(new User(userName().toLowerCase(), email(), password(), role));
+    }
+
+    if (users.length === 1) {
+        return users[0]
+    }
+
+    return users;
+}
+
+const generatePosts = (count: number) => {
+    const { sentence, paragraph } = faker.lorem;
+    const { city } = faker.image;
+
+    let posts = [];
+
+    for (let index = 0; index < count; index++) {
+        posts.push(new Post(sentence(), paragraph(), city(640, 480, true), 1));
+    }
+
+    if (posts.length === 1) {
+        return posts[0]
+    }
+
+    return posts;
+}
+
+const users = generateUsers(10);
+const posts = generatePosts(10);
+
+const createPosts = seedMany(postsTable);
+const createUsers = seedMany(usersTable);
+
+const catchErr = (e: Error) => console.error(e);
+const disconnect = () => prisma.$disconnect();
+
+createPosts(posts).catch(catchErr).finally(disconnect);
+createUsers(users).catch(catchErr).finally(disconnect);
