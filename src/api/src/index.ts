bcebf54057eda31d9f702424b7df03018fd19a6c
Peter Pumpkineater <n1-dev+pumpkineater6996@simplelogin.co>
2023-01-21 01:37:49 -0500

    Remove session cookie
    Removed in favor of token based authentication.

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -10,23 +10,7 @@ import postsRouter from './routes/PostRoutes';
 import usersRouter from './routes/UserRoutes';
 import authRouter from './routes/AuthRoutes';
 
-import { generateUserPassword } from './util/authentication/authenticationFunctions';
-
-declare module 'express-session' {
-    export interface SessionData {
-        user: { [key: string]: any } | null;
-    }
-}
-
 const app = express();
-
-const secretHash = generateUserPassword('myawesomeblog', bcrypt.genSaltSync());
-
-app.use(session({
-    secret: secretHash,
-    resave: false,
-    saveUninitialized: true
-}));
 
 app.use(cors());
 app.use(json())
