45d507ec7726df5a9c39ee97ee4e45030af97c50
Peter Pumpkineater <n1-dev+pumpkineater6996@simplelogin.co>
2023-01-25 22:27:55 -0500

    Update layout

--- src/client/src/pages/Dashboard/Dashboard.tsx
+++ src/client/src/pages/Dashboard/Dashboard.tsx
@@ -22,15 +22,15 @@ function Dashboard() {
 		<PageWrapper className='relative'>
 			<PageTitle title='Dashboard' />
 
-			<aside className='flex flex-col fixed right-0'>
-				<section className='flex flex-col gap-6 bg-gray-200 p-5 flex-1'>
-					<span>Dashboard</span>
-					<span>Posts</span>
-					<span className='mt-auto'>Settings</span>
+			<aside className='flex flex-col fixed right-44 top-44'>
+				<section className='flex flex-col gap-6 bg-gray-100 shadow-lg px-6 py-4 flex-1 rounded-lg'>
+					<span>My Posts</span>
+					<span>Profile</span>
 				</section>
 			</aside>
 
-			<section className='flex flex-col gap-2 p-5 col-span-3'>
+			<section className='flex flex-col gap-2'>
+				<h3 className='font-bold text-2xl'>My Posts</h3>
 				{posts.map(post => (
 					<BlogPostListItem item={post} key={uuidv4()} />
 				))}
