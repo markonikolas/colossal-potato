043e537a0ab21a333aaac70643dddc1fa2c1e6cc
Peter Pumpkineater <n1-dev+pumpkineater6996@simplelogin.co>
2023-01-25 22:17:17 -0500

    Add title and page wrapper components to Dashboard component

--- src/client/src/pages/Dashboard/Dashboard.tsx
+++ src/client/src/pages/Dashboard/Dashboard.tsx
@@ -3,6 +3,8 @@ import { useContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import BlogPostListItem from '../Blog/BlogPostListItem';
+import PageWrapper from '../../components/Layout/PageWrapper';
+import PageTitle from '../../components/Layout/PageTitle';
 
 function Dashboard() {
 	const [posts, setPosts] = useState([]);
@@ -17,20 +19,23 @@ function Dashboard() {
 	}, [fetchBlogs]);
 
 	return (
-		<main className='grid grid-cols-4 gap-4 h-body'>
-			<aside className='flex flex-col'>
+		<PageWrapper className='relative'>
+			<PageTitle title='Dashboard' />
+
+			<aside className='flex flex-col fixed right-0'>
 				<section className='flex flex-col gap-6 bg-gray-200 p-5 flex-1'>
 					<span>Dashboard</span>
 					<span>Posts</span>
 					<span className='mt-auto'>Settings</span>
 				</section>
 			</aside>
+
 			<section className='flex flex-col gap-2 p-5 col-span-3'>
 				{posts.map(post => (
 					<BlogPostListItem item={post} key={uuidv4()} />
 				))}
 			</section>
-		</main>
+		</PageWrapper>
 	);
 }
 
