f7d717a7262819f4581dfc73d7a70f6e77a79405
Peter Pumpkineater <n1-dev+pumpkineater6996@simplelogin.co>
2023-01-27 22:36:03 -0500

    Update BlogPost presentation

--- src/client/src/pages/Blog/BlogPost.tsx
+++ src/client/src/pages/Blog/BlogPost.tsx
@@ -4,7 +4,7 @@ function BlogPost({ item }: BlogPostProps) {
 	const { title, excerpt, img } = item;
 
 	return (
-		<article className='min-w-full max-w-md mx-auto mt-4 shadow-md rounded-md duration-300 hover:shadow-xl cursor-pointer'>
+		<article className='min-w-full max-w-md mx-auto mt-4 overflow-hidden rounded-xl duration-700 cursor-pointer shadow-xl shadow-indigo-50 md:shadow-gray-100 hover:shadow-3xl md:hover:-translate-y-3 md:hover:shadow-indigo-50'>
 			<a href='/'>
 				<img
 					src={img}
@@ -12,11 +12,12 @@ function BlogPost({ item }: BlogPostProps) {
 					alt={title}
 					className='flex items-center justify-center w-full h-48 rounded-t-md bg-gray-200 before:text-gray-300 before:font-bold before:text-2xl before:tracking-wide object-cover'
 				/>
+
 				<div className='flex items-center mt-2 pt-3 ml-4 mr-2'>
-					<div className='flex-none w-10 h-10 rounded-full'>
+					<div className='flex-none w-10 h-10 rounded-full overflow-hidden'>
 						<img
 							src=''
-							className='flex items-center justify-center overflow-hidden bg-gray-300 before:text-gray-400 before:text-xs before:text-center before:opacity-0 w-full h-full rounded-full'
+							className='flex items-center justify-center overflow-hidden bg-gray-300 before:text-gray-400 before:text-xs before:text-center before:opacity-0 w-full h-full'
 							alt=''
 						/>
 					</div>
@@ -25,6 +26,7 @@ function BlogPost({ item }: BlogPostProps) {
 						<span className='block text-gray-400 text-sm'>04-04-2022</span>
 					</div>
 				</div>
+
 				<div className='pt-3 ml-4 mr-2 mb-3'>
 					<h3 className='text-xl text-gray-900'>{title}</h3>
 					<p className='text-gray-400 text-sm mt-1'>{excerpt}</p>
