40e7329943eabb559775d4d795dbdcc9fb0fc5e9
Peter Pumpkineater <n1-dev+pumpkineater6996@simplelogin.co>
2023-01-28 14:58:24 -0500

    Fix logo alignment

--- src/client/src/components/Layout/Header.tsx
+++ src/client/src/components/Layout/Header.tsx
@@ -13,7 +13,7 @@ const Header: FC<IHeaderProps> = () => {
 		<header className='grid overflow-hidden'>
 			<div className='flex justify-self-center justify-between items-center max-w-7xl w-full p-8'>
 				<div id='logo'>
-					<Link to='/' className='text-sm font-bold tracking-wide rounded-lg py-2 px-4 ring-blue-500  transition-colors'>
+					<Link to='/' className='-ml-4 text-sm font-bold tracking-wide rounded-lg py-2 px-4 ring-blue-500  transition-colors'>
 						MyBlog
 					</Link>
 				</div>
